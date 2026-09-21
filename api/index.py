757b2b7698767bb42a9545011863891927e07db8
doberobo <COLOCA_SEU_EMAIL_REAL_AQUI@gmail.com>
2026-09-21 09:17:17 -0400

    fix: api ok json

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
 ﻿from http.server import BaseHTTPRequestHandler
-
+import json
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write(b'API OK')
+        self.wfile.write(json.dumps({"ok": True, "api": "online"}).encode())
         return
